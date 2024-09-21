2f03fb93d88c31d498ebb9b887e4c5b6706d6c44
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-09-21 00:51:08 -0500

    Adding movement to aliens

--- G1_Challenge01_VXR/Assets/Scripts/LookTowardsPlayer.cs
+++ G1_Challenge01_VXR/Assets/Scripts/LookTowardsPlayer.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class LookTowardsPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
     public float rotateSpeed = 5f;
+    public float moveRadious = 25f;
+    public float speed = 1f;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -18,9 +21,23 @@ public class LookTowardsPlayer : MonoBehaviour
     void Update()
     {
         Vector3 direction = player.transform.position - transform.position;
+        float distanceToAlien = Vector3.Distance(player.transform.position, transform.position);
         Quaternion lookRotation = Quaternion.LookRotation(direction);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed*Time.deltaTime);
+
+        if (distanceToAlien <= moveRadious) 
+        {
+            if (distanceToAlien > 5f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            }
+            else 
+            {
+                Vector3 moveAwayPosition = transform.position - direction.normalized * speed * Time.deltaTime;
+                transform.position = moveAwayPosition;
+            }
+        }
         
     }
 }
