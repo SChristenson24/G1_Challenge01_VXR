228ad9effc924a54731b19d928fd0939d76367ab
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-09-22 23:05:54 -0500

    Comments

--- G1_Challenge01_VXR/Assets/Scripts/LookTowardsPlayer.cs
+++ G1_Challenge01_VXR/Assets/Scripts/LookTowardsPlayer.cs
@@ -21,10 +21,12 @@ public class LookTowardsPlayer : MonoBehaviour
 
     void Start()
     {
+        //Getting the player object using tags
         player = GameObject.FindGameObjectWithTag("Player");
 
         alienName = transform.name.Replace("(Clone)", "");
-        Debug.Log(alienName);
+
+        //Freaky Alien doesn't have animations, so this if statement makes sure we don't call on animator for it
         if (alienName != "Freaky Alien")
         {
             animator = GetComponent<Animator>();
@@ -35,24 +37,37 @@ public class LookTowardsPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.transform.position - transform.position;
+        Vector3 direction = player.transform.position - transform.position; //Gets the direction alien has to move
         float distanceToAlien = Vector3.Distance(player.transform.position, transform.position);
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
 
+        //Code to make the alien look towards the player
+        Quaternion lookRotation = Quaternion.LookRotation(direction); 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed*Time.deltaTime);
+
+        //Moving towards alien if outside a certain radious, but inside another
+        //Not moving if colliding because that creates issues with animations and collisions
         if (distanceToAlien <= moveRadious && distanceToAlien > 5f && !isColliding)
         {
             if (alienName != "Freaky Alien")
             {
+                //Setting the animation true when moving towards player
                 animator.SetBool("isMoving", true);
             }
+
+            //Moving towards player
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
         else
         {
+            //Else statement when not moving
+            //Alien is near the player now
+            //------ Add the sounds here ------ 
             if (alienName != "Freaky Alien")
             {
+                //Setting moving animations false
                 animator.SetBool("isMoving", false);
+
+                //This code block is for making the aliens have random animations and actions sometimes
                 timeSinceLastAction += Time.deltaTime;
 
                 if (timeSinceLastAction >= cooldownTime)
@@ -78,6 +93,7 @@ public class LookTowardsPlayer : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        //Detecting Collisions
         if (collision.gameObject.CompareTag("Alien"))
         {
             isColliding = true;
